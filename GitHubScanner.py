e0eb03bd793caa37626556e16789424b24f9800c
mariadalid <20243rd014@utez.edu.mx>
2025-09-17 16:19:39 -0600

    GitHubScanner: VALIDACION

--- GitHubScanner.py
+++ GitHubScanner.py
@@ -9,3 +9,13 @@ git_dir = r"C:\Users\Airami\OneDrive\Desktop\Programacion_de_redes"
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir, name)) and
          os.path.exists(os.path.join(git_dir, name, ".git"))]
+
+
+if repos:
+    print("Found the following GitHub repositor")
+    for idx, repo in enumerate(repos, 1):
+        print(f"{idx}, {repo}")
+
+    else:
+        print("No Git Hub repositiries found in")
+        print("Please provide a GitHub repository")
